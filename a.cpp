a8970206a6c6f2ac8b9de88261793e90bcde7797
Abhi-kr21 <abhishek7858010345@gmail.com>
2022-06-24 18:41:56 +0530

    my second commit

--- a.cpp
+++ a.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 int main()
 {
-    cout<<"This is my 2nd conmmit";
-    return 0;
+int n;
+scanf("%d",&n);
+int a[n];
+for(int i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(int i=n-1;i>=0;i--)
+printf("%d ",a[i]);
+return 0;
 }
